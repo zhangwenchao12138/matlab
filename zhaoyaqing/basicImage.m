40b5b86c01e3768034ac4524d5381ba995743d11
WenchaoZhang <18003916404@163.com>
2019-05-29 23:21:50 +0800

    update

--- zhaoyaqing/basicImage.m
+++ zhaoyaqing/basicImage.m
@@ -1,25 +1,33 @@
 clear all; clc;
+
 Image = imread('J069_089.tif');
-R=Image(:,:,1); G=Image(:,:,2); B=Image(:,:,3);
 
-Gray=rgb2gray(Image);
+imageR = Image(:,:,1); 
+imageG = Image(:,:,2); 
+imageB = Image(:,:,3);   %做3张RGB分图
 
-R=Image(:,:,1); G=Image(:,:,2); B=Image(:,:,3);
+Gray=rgb2gray(Image);       %转化为灰度图
 
-diff_R=0; diff_G=20; diff_B=0;  % 设置红、绿、蓝三种颜色提取阈值（越大越严格）
+diff_R=0; diff_G=20; diff_B=0;  % 设置红、绿、蓝三种颜色提取阈值
 
-Image_G=Image;
+ImageGreen = Image;
 
-GP_R=Image(:,:,1); GP_G=Image(:,:,2); GP_B=Image(:,:,3);
+GP_R = Image(:,:,1); 
+GP_G = Image(:,:,2); 
+GP_B = Image(:,:,3);
 
-XYG=~((G-R)>diff_G&(G-B)>diff_G);  % 提取绿色条件是G分量与R、B分量差值大于设定
+XYG = ~((imageG-imageR)>diff_G&(imageG-imageB)>diff_G);  % 提取绿色条件是G分量与R、B分量差值大于设定
 
-Mask=Gray(XYG);  % 灰照片掩膜
+MaskGray=Gray(XYG);  % 灰照片掩膜
 
-GP_R(XYG)=Mask; GP_G(XYG)=Mask; GP_B(XYG)=Mask;  % 使得非绿色区域变为灰色
+GP_R(XYG)=MaskGray; 
+GP_G(XYG)=MaskGray; 
+GP_B(XYG)=MaskGray;  % 使得非绿色区域变为灰色
 
-Image_G(:,:,1)=GP_R; Image_G(:,:,2)=GP_G; Image_G(:,:,3)=GP_B;
+ImageGreen(:,:,1)=GP_R; 
+ImageGreen(:,:,2)=GP_G; 
+ImageGreen(:,:,3)=GP_B;
 
 
-imshow(Image_G(:,:,1) );
+imshow(ImageGreen);
 
